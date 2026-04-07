aefe04af73b8addd7d3e038fb206097901fd4a61
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-04-08 01:52:05 +0600

    begining

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-6xl'>Hello</h1>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
